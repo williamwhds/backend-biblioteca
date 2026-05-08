58486f1df8e6e64b829e5d26ab47dba32857f0ea
William Oliveira <202211240022@ifba.edu.br>
2026-05-08 13:38:32 -0300

    feat: library app views

--- library/views.py
+++ library/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from .models import Book, Borrower, Loan
+from .serializers import BookSerializer, BorrowerSerializer, LoanSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Book.objects.filter(added_by=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(added_by=self.request.user)
+
+
+class BorrowerViewSet(viewsets.ModelViewSet):
+    serializer_class = BorrowerSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Borrower.objects.filter(added_by=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(added_by=self.request.user)
+
+
+class LoanViewSet(viewsets.ModelViewSet):
+    serializer_class = LoanSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Loan.objects.filter(added_by=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(added_by=self.request.user)
