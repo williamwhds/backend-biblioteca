fdf2e3684a176995d01811721d93432350f7cfa6
William Oliveira <202211240022@ifba.edu.br>
2026-05-08 13:48:18 -0300

    wip: setting up auth

--- bibliotecabackend/settings.py
+++ bibliotecabackend/settings.py
@@ -38,9 +38,19 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework.authtoken",
     "accounts",
     "library",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
