2b9750806b069fee9898927a92e951b760f699f6
William Oliveira <202211240022@ifba.edu.br>
2026-05-08 13:04:40 -0300

    chore: register models to dashboard

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from library.models import Book, Borrower, Loan
+
+admin.site.register(Book)
+admin.site.register(Loan)
+admin.site.register(Borrower)
